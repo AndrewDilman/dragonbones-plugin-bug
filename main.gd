7b2e29f4e15cbc94bf9b652f5795b7f256f22ab6
AndrewD <AndrewDilman@users.noreply.github.com>
2025-04-24 00:24:36 +0700

    tried to reproduce errors
    https://github.com/Daylily-Zeleen/Godot-DragonBones/issues/48

---
+++ main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await get_tree().create_timer(2).timeout
+	$DragonBonesArmatureView/AnimationPlayer.play("Default")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
